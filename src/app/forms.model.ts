e23aecc31f8449ab2a22d3bf79d52892e0fb78b8
Mori-Chan <59697686+Mori-Chan@users.noreply.github.com>
2021-01-23 14:00:45 +0900

    aaaa

---
+++ src/app/forms.model.ts
@@ -0,0 +1,13 @@
+export class Forms {
+  constructor(
+    public your_name: string,
+    public furigana: string,
+    public email: string,
+    public email_check: string,
+    public contact_type: string,
+    public subject: string,
+    public comment: string,
+    public tel?: string,
+    public companyname?: string
+  ){}
+}
