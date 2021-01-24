93857330c841a627abb577ae7901d60109ac9a40
yama-ren <renren@sanbakaraisu.com>
2021-01-24 10:11:33 +0900

    20200124

--- src/app/employee-detail5/employee-detail5.component.ts
+++ src/app/employee-detail5/employee-detail5.component.ts
@@ -1,13 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ɵɵNgOnChangesFeature} from '@angular/core';
+import { EmployeeModel, contact-types } from './employee-list.service';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmployeeModel, contact-types } from './employee.model';
+
+
 
 @Component({
   selector: 'app-employee-detail5',
   templateUrl: './employee-detail5.component.html',
-  styleUrls: ['./employee-detail5.component.css']
 })
-export class EmployeeDetail5Component implements OnInit {
+export class EmployeeDetail5Component {
+  @Input() employee: EmployeeModel;
+  employeeForm: FormGroup;
+  contact-types = contact-types;
 
-  constructor() { }
+  constructor(public fb: FormBuilder, public employeeListService: EmployeeListService) {
+    this.createForm();
+   }
+
+   createForm() {
+     this.employeeForm = this.fb.group({
+       id: '',
+       name: ['', Validators.required],
+       phone: ['', Validators.required],
+       department: [this.contact-types[0], Validators.required],
+       email: ''
+     });
+
+     ngOnChanges(){
+       this.employeeForm.reset(
+         this.employee
+       );
+     }
+
+     save(event){
+       event.preventDefault();
+       this.employeeListService.updateEmployee(this.employeeForm.value);
+     }
+   }
 
   ngOnInit(): void {
   }
