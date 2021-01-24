f2bb3c6fc25f5ab7ff1f99f70ef5e81ac2a66a3a
Mori-Chan <59697686+Mori-Chan@users.noreply.github.com>
2021-01-24 10:02:32 +0900

    もりちゃんemployee-detail4

--- src/app/employee-detail4/employee-detail4.component.ts
+++ src/app/employee-detail4/employee-detail4.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Forms, contact_types } from '../forms.model' ;
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-employee-detail4',
@@ -7,7 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeDetail4Component implements OnInit {
 
-  constructor() { }
+  @Input() employee: Forms;
+  employeeForm: FormGroup;
+  contact_types = contact_types;
+
+  constructor(public fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm(){
+    this.employeeForm = this.fb.group({
+      id: '',
+      name: ['', Validators.required ],
+      phone: ['', Validators.required ],
+      contact_types: [this.contact_types[0], Validators.required ],
+      email: ''
+    });
+  }
+  ngOnChanges() {
+    this.employeeForm.reset(
+    this.employee
+    );
+  }
 
   ngOnInit(): void {
   }
