8126873f29d1eb27c40082f5e8e05abb8dc7c525
panpanYuya <yuya@sanbakaraisu.com>
2021-01-24 09:06:44 +0900

    最新版

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployDetailComponent } from './employ-detail/employ-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    EmployDetailComponent
   ],
   imports: [
     BrowserModule,
