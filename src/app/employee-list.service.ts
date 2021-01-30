7c4952a247304044fa1351299c714765156ed114
Mori-Chan <59697686+Mori-Chan@users.noreply.github.com>
2021-01-30 17:13:46 +0900

    ghflifgil;f

--- src/app/employee-list.service.ts
+++ src/app/employee-list.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
+import { Observable } from 'rxjs';
+import { Forms, formsContent } from './forms.model';
+@Injectable()
 export class EmployeeListService {
-
-  constructor() { }
+ list$: Observable<any>;
+ selectedEmployee: Forms;
+ private _observer;
+ constructor() {
+ this.list$ = new Observable((observer) => {
+ this._observer = observer;
+ });
+ }
+ getList() {
+ this._observer.next(formsContent);
+ }
+ updateEmployee(employee: Forms) {
+ const oldEmployee = formsContent.find(e => e.your_name === employee.your_name);
+ const newEmployee = Object.assign(oldEmployee, employee);
+ this._observer.next(formsContent);
+ }
 }
